9a71deb281d926d69b0d75cb1d5ea607ca71cf5b
amithshibu <amithshibu2002@gmail.com>
2024-02-22 10:55:05 +0530

    hello hi

---
+++ qq.java
@@ -0,0 +1,13 @@
+public class qq {
+    public static void main(String[] args) {
+        // Define two numbers
+        int num1 = 3;
+        int num2 = 4;
+        
+        // Calculate the sum
+        int sum = num1 + num2;
+        
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
